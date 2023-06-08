f3790d7849b7dde443b543fbaba39c22058c92a1
jeanc <jeancarlocldrn@gmail.com>
2023-06-08 17:26:51 -0600

    -Interfaz Adjacency List Graph, falta funcionalidad

---
+++ src/main/java/controller/AdjListGraphController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+
+public class AdjListGraphController {
+
+    @javafx.fxml.FXML
+    private Pane graphPane;
+
+    @javafx.fxml.FXML
+    private TextArea listGraphTextArea;
+
+    @javafx.fxml.FXML
+    public void initialize() {}
+
+    @javafx.fxml.FXML
+    void bfsTourOnAction(ActionEvent event) {
+
+    }
+
+    @javafx.fxml.FXML
+    void containsEdgeOnAction(ActionEvent event) {
+
+    }
+
+    @javafx.fxml.FXML
+    void containsVextexOnAction(ActionEvent event) {
+
+    }
+
+    @javafx.fxml.FXML
+    void dfsTourOnAction(ActionEvent event) {
+
+    }
+
+    @javafx.fxml.FXML
+    void randomizeOnAction(ActionEvent event) {
+
+    }
+
+    @javafx.fxml.FXML
+    void toStringOnAction(ActionEvent event) {
+
+    }
+}
