e109e73f1184a770aad71300aea1e1ed8e8b4dae
jeanc <jeancarlocldrn@gmail.com>
2023-06-09 20:26:35 -0600

    -Punto 6b completo

--- src/main/java/controller/AdjListGraphController.java
+++ src/main/java/controller/AdjListGraphController.java
@@ -98,7 +98,7 @@ public class AdjListGraphController {
         for (int i = 0; i < n; i++) {
             int vertexIndex = util.Utility.random(9);
             int vertexIndex2 = util.Utility.random(9);
-            int peso = util.Utility.random(99);
+            int peso = util.Utility.random(0,50);
             if (!adj.containsEdge(adj.getVertex(vertexIndex2).data,adj.getVertex(vertexIndex).data)) {
                 adj.addEdgeAndWeight(adj.getVertex(vertexIndex2).data, adj.getVertex(vertexIndex).data, peso);
                 //System.out.println(vertexIndex+", "+vertexIndex2); //prueba
